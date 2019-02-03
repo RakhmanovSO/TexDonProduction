cac5c1ad65d3ea5a5c00486f73058315b62664d2
RakhmanovSO <42904303+RakhmanovSO@users.noreply.github.com>
2019-02-03 04:58:50 +0300

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,12 @@ module.exports = {
     mode: "development",
     devtool: "source-map",
     entry: "./application/app.js",
-    watch: false,
     output: {
         filename: "index.js",
         path: path.resolve(__dirname, "public/js")
     },
-    //watch: true
 
+    watch: false
+
+    //watch: true
 };
